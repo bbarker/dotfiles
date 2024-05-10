6fa54a457920b06fdb5554ea1f20a5f472220fc5
Brandon Barker <brandon.barker@gmail.com>
2024-05-10 07:33:50 -0400

    add alacritty for nixos

--- .config/home-manager/home-x86_64-Linux-nixos.nix
+++ .config/home-manager/home-x86_64-Linux-nixos.nix
@@ -3,10 +3,11 @@
 let
   common = import ./home-common.nix { inherit inputs config pkgs; };
   waylandHome = import ./wayland.nix { inherit inputs config pkgs; };
+  nixosHome = import ./nixos.nix { inherit inputs config pkgs; };
 in
 {
   imports = [ common ];
-  home.packages = waylandHome.packages ++ common.home.packages;
+  home.packages = waylandHome.packages ++ common.home.packages ++ nixosHome.packages;
 
   # Home Manager needs a bit of information about you and the
   # paths it should manage.
